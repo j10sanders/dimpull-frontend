83adbc778137df9b170ff1f40e453e30e248c27e
j10sanders <jonsandersss@gmail.com>
2018-03-09 09:15:20 -0500

    Change from creat react app to dimpull and fix footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,13 +11,13 @@ export const Footer = () =>
 			        <img src='https://res.cloudinary.com/dtvc9q04c/image/upload/v1519823675/orangemagnet-48.png' 
 			        style={{width: "40px",height: "auto", paddingBottom: '13px'}} alt="logo" />
 		        
-		        <div style={{float:'right', position:'absolute', bottom:'0', fontSize: '17px', right: '7px'}}>
-		        <div style={{display: 'inline-block', paddingBottom: "3px"}}>
+		        <div style={{float:'right', position:'absolute', bottom:'0', fontSize: '13px', right: '7px'}}>
+		        <div style={{paddingBottom: "13px", paddingLeft: "19px"}}>
 		        <a href="mailto:admin@dimpull.com" >
 		        	admin@dimpull.com
 		        </a>
 		        </div>
-		        <div style={{display: 'inline-block', paddingLeft: '24px'}}>
+		        <div style={{display: 'inline-block', paddingLeft: '24px', paddingBottom: '4px'}}>
 		        <Follow username='dimpull'  options={{showCount:false, size: 'large'}} />	
 		        </div>
 		        </div>			
