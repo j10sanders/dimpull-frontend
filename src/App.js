ddbf4b33ac4fdfb28d84de5713d8c438deecdc19
j10sanders <jonsandersss@gmail.com>
2018-03-15 21:50:51 -0400

    maybe it was a redirect issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-// import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-// import * as Colors from 'material-ui/styles/colors';
-// import { fade } from 'material-ui/utils/colorManipulator'
 import React from 'react';
 import './App.css';
-// import darkBaseTheme from 'material-ui/styles/getMuiTheme';
-// import {deepOrange500} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Routes from './routes';
 import { Footer } from './components/Footer';
-
 require('bootstrap-webpack');
 
 
@@ -29,18 +23,15 @@ const muiTheme = getMuiTheme({
 
 
 class App extends React.Component {
-    
-    // <Footer />
-
     render() {
         return (
             <div id="othersection">
-            <MuiThemeProvider muiTheme={muiTheme} style={styles}>
-                <section id='section'>
-                    <Routes />
-                </section>
-            </MuiThemeProvider>
-            <Footer />
+                <MuiThemeProvider muiTheme={muiTheme} style={styles}>
+                    <section id='section'>
+                        <Routes />
+                    </section>
+                </MuiThemeProvider>
+                <Footer />
             </div>
         );
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,6 @@ import { LoginPage } from '../LoginPage';
 import { GetNumber } from '../LoginPage/GetNumber';
 import Home from '../components/Home';
 import { Header } from '../components/Header';
-// import { Footer } from '../components/Footer'
 import Auth from '../Auth/Auth.js';
 import history from '../history';
 import Callback from '../Callback/Callback';
@@ -32,47 +31,47 @@ require('bootstrap-webpack');
 class Routes extends Component {
 	render() {
 		return (
-				<Router history={history}>
-					<div>
-						<Header />
-						<div
-							className="container"
-	                        style={{ paddingBottom: '100px',
-	                        paddingTop: '64px',
-		                        // height: '100%',
-		                        // minHeight: '88vh'
-	                        }}
-                      	>
-							<Switch>
-								<Route exact path="/" render={(props) => <Home auth={auth} {...props} />} />
-								<Route exact path="/login" render={(props) => <LoginPage auth={auth} {...props} />} />
-								<Route exact path="/getNumber" render={(props) => <GetNumber auth={auth} {...props} />} />
-							    <Route exact path="/discussions" render={(props) => <Discussions auth={auth} {...props} />} />
-							    <Route exact path="/mydiscussions" render={(props) => <MyDiscussions auth={auth} {...props} />} />
-							    <Route path="/discussionProfile" render={(props) => <DiscussionProfile auth={auth} {...props} />} />
-							    <Route path="/requestConversation" render={(props) => <Contact auth={auth} {...props} />} />
-							    <Route path="/newProfile" render={(props) => <NewProfile auth={auth} {...props} />} />
-							    <Route path="/editProfile" render={(props) => <EditProfile auth={auth} {...props} />} />
-							    <Route path="/availability" render={(props) => <Availability auth={auth} {...props} />} />
-							    <Route path="/calendar" render={(props) => <Calendar auth={auth} {...props} />} />
-							    <Route path="/pay" render={(props) => <Pay auth={auth} {...props} />} />
-							    <Route path="/profile" render={(props) => (
-						            !auth.isAuthenticated() ? (
-						              <Redirect to="/home"/>
-						            ) : (
-						              <Profile auth={auth} {...props} />
-						            )
-						          )} />
-							    <Route path="/callback" render={(props) => {
-						          handleAuthentication(props);
-						          return <Callback {...props} /> 
-						        }}/>
-						    </Switch>
+			<Router history={history}>
+				<div>
+					<Header />
+					<div
+						className="container"
+                        style={{ paddingBottom: '100px',
+                        paddingTop: '64px',
+	                        // height: '100%',
+	                        // minHeight: '88vh'
+                        }}
+                  	>
+						<Switch>
+							<Route exact path="/" render={(props) => <Home auth={auth} {...props} />} />
+							<Route exact path="/login" render={(props) => <LoginPage auth={auth} {...props} />} />
+							<Route exact path="/getNumber" render={(props) => <GetNumber auth={auth} {...props} />} />
+						    <Route exact path="/discussions" render={(props) => <Discussions auth={auth} {...props} />} />
+						    <Route exact path="/mydiscussions" render={(props) => <MyDiscussions auth={auth} {...props} />} />
+						    <Route path="/discussionProfile" render={(props) => <DiscussionProfile auth={auth} {...props} />} />
+						    <Route path="/requestConversation" render={(props) => <Contact auth={auth} {...props} />} />
+						    <Route path="/newProfile" render={(props) => <NewProfile auth={auth} {...props} />} />
+						    <Route path="/editProfile" render={(props) => <EditProfile auth={auth} {...props} />} />
+						    <Route path="/availability" render={(props) => <Availability auth={auth} {...props} />} />
+						    <Route path="/calendar" render={(props) => <Calendar auth={auth} {...props} />} />
+						    <Route path="/pay" render={(props) => <Pay auth={auth} {...props} />} />
+						    <Route path="/profile" render={(props) => (
+					            !auth.isAuthenticated() ? (
+					              <Redirect to="/home"/>
+					            ) : (
+					              <Profile auth={auth} {...props} />
+					            )
+					          )} />
+						    <Route path="/callback" render={(props) => {
+					          handleAuthentication(props);
+					          return <Callback {...props} /> 
+					        }}/>
+					    </Switch>
 
-					    </div>
-					</div>
+				    </div>
+				</div>
 
-				</Router>
+			</Router>
 		);
 	}
 }
