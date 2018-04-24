f1e4dd2c1d85d18c780e0fa57a02bfd5319634e8
j10sanders <jonsandersss@gmail.com>
2018-04-24 19:59:22 -0400

    working on ropsten network

--- src/truffle.js
+++ src/truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic ="lens ladder destroy burden scout payment trade alley sorry arrange top earn";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -8,7 +11,15 @@ module.exports = {
       port: 9545,
       network_id: "*",
       gas: 30000000 // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/<INFURA_Access_Token>")
+      },
+      network_id: 3,
+      gas: 4612388
     }
+
   },
   solc: { optimizer: { enabled: true, runs: 200 } }
 };
