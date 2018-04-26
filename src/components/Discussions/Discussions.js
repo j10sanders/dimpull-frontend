82111a8ed06a466a5a568b17c5ee2f89ee457c58
j10sanders <jonsandersss@gmail.com>
2018-04-26 12:26:28 -0400

    meet the experts using a list and looking alright

--- src/components/Discussions/Discussions.js
+++ src/components/Discussions/Discussions.js
@@ -1,45 +1,23 @@
 import React from 'react';
-import {GridList, GridTile} from 'material-ui/GridList';
-// import IconButton from 'material-ui/IconButton';
-import Subheader from 'material-ui/Subheader';
-// import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 import CircularProgress from 'material-ui/CircularProgress';
-// import { Home } from '../../components/Home';
-import RaisedButton from 'material-ui/RaisedButton';
-// import FontIcon from 'material-ui/FontIcon';
-// import SvgIcon from 'material-ui/SvgIcon';
-import Divider from 'material-ui/Divider';
-// import Paper from 'material-ui/Paper';
-// import './landingpage.css';
+import { Link } from 'react-router-dom';
+import Paper from 'material-ui/Paper';
+import ReactStars from 'react-stars';
+import { darkBlack } from 'material-ui/styles/colors';
 
-// const HomeIcon = (props) => (
-//   <SvgIcon {...props}>
-//     <path d="M100.1 72L64 93.7 27.9 72c-.8-.5-1.9-.3-2.5.4-.6.7-.7 1.7-.2 2.5l37.2 52.2.1.1c.1.1.1.2.2.2l.1.1c.1.1.2.1.2.2 0 0 .1 0 .1.1.1 0 .2.1.3.1h.1c.1 0 .3.1.4.1.2 0 .3 0 .4-.1h.1c.1 0 .2-.1.3-.1 0 0 .1 0 .1-.1.1 0 .2-.1.2-.2l.1-.1.2-.2.1-.1 37.2-52.2c.5-.8.5-1.8-.2-2.5s-1.4-.8-2.3-.4zM62 97.1v22.6L34 80.4l28 16.7zm4 22.6V97.1l28-16.8-28 39.4z"/>
-//   </SvgIcon>
-// );
+import './discussionprofile.css';
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    width: '100%',
-    height: '500px',
-  },
-  gridList: {
-    width: 800,
-    // overflowY: 'hidden',
-  },
-};
 
 class Discussions extends React.Component {
 	constructor(props) {
     super(props);
-    this.state = {dps: null,
+    this.state = {
+      dps: [],
       waiting: true
-                  };
+    };
   }
 
   async getDiscussions(){
@@ -65,41 +43,44 @@ class Discussions extends React.Component {
     return (
       <div style={{textAlign: 'center'}}>
       <CircularProgress style={{display: waiting, width: '100%'}} size={80} thickness={5} />
-        <h1>Meet the Experts</h1>
-        
-        <div style={styles.root}>
-        {this.state.dps && (
-    	    <GridList
-    	      cellHeight={180}
-            id="GridlistID"
-    	      style={styles.gridList}
-            cols={3}
-    	    >
-          <Subheader>These are example profiles while we prepare our first group of experts</Subheader>
-          
-  	      {this.state.dps.map((dp) => (
-            <Link to={`/expert/${dp.url}`} key={dp.url}>
-  	        <GridTile
-  	          key={dp.id}
-  	          title={<b>{`${dp.first_name} ${dp.last_name}`}</b>}
-  	          subtitle={dp.description}
-  	        >
-  	          <img src={dp.image} alt={dp.id} />
-  	        </GridTile>
-            </Link>
-  	      ))}
-        </GridList>
-      )}
-      </div>
-      <Divider style={{marginTop: '80px'}}/>
-          <h2 style={{paddingTop: '20px'}}> Are You an Expert? </h2>
-          <RaisedButton
-            containerElement={<Link to="/newProfile"  />}
-            label="Become a Dimpull Expert"
-            secondary={true}
-            style={{marginTop: '20px', marginBottom: '20px'}}
-            />
-          
+        <div style={{ marginTop: '50px' }}>
+          <h1>Meet the Experts</h1>
+        </div>
+        <div id="meetExperts">
+          <Paper>
+            <List>
+              {this.state.dps.map((dp) => (
+          	    <ListItem
+                  leftAvatar={<Avatar src={dp.image} style={{border: 0, objectFit: 'cover'}}/>}
+                  key={dp.id}
+                  containerElement={<Link to={`/expert/${dp.url}`} key={dp.url}/>}
+                  primaryText={`${dp.first_name} ${dp.last_name}`}
+                  secondaryText={<p><span style={{color: darkBlack}}>{dp.description} </span>{dp.who}</p>}
+                  style={{ textAlign: 'left' }}
+                  secondaryTextLines={2}
+                  leftIcon={
+                    <div
+                      style={{
+                        float: 'right', margin: 'auto', position: 'inherit', width: 'auto'
+                      }}
+                    >
+                    <div style={{marginTop:'-10px', paddingBottom:'5px', textAlign: 'center' }}>$50</div>
+                      {dp.averageRating ? (<ReactStars
+                        count={5}
+                        size={24}
+                        color2="#ffd700"
+                        value={dp.averageRating}
+                        // half={false}
+                        edit={false}
+                      />
+                      ) : (<div style={{ fontSize: '17px', color: 'darkgray' }}> No reviews yet </div>)}
+                    </div>
+                  }
+          	    />
+              ))}
+            </List>
+          </Paper>
+        </div>
     </div>
     );
   }
