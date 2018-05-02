982041e597a1207b2a14b98e7bb4abebe06f6a88
j10sanders <jonsandersss@gmail.com>
2018-05-02 09:23:37 -0400

    put profiles in same order

--- src/components/Home/DefaultProfiles.js
+++ src/components/Home/DefaultProfiles.js
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 export default function DefaultProfiles () {
   const dps = [
     {
-      id: 1, url: 'ben-way', first_name: 'Ben', last_name: 'Way', description: 'Crypto/ICO expert with extensive technical background', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,h_595/v1523898203/experts/byfzhf14drmapbugfq7n.png'
+      id: 1, url: 'tlc', first_name: 'Toni', last_name: 'Lane', description: 'Founder @ Cultu.re / Co-founder @ CoinTelegraph | Blockchain Pioneer | Entrepreneur | Investor | Empath | Board Member | Transformational Evangelist | Happy Lady.', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,f_auto,h_595,q_auto:eco/v1523635339/experts/sdo9fgjmzhsjvm2xyqhm.jpg'
     },
     {
-      id: 2, url: 'tlc', first_name: 'Toni', last_name: 'Lane', description: 'Founder @ Cultu.re / Co-founder @ CoinTelegraph | Blockchain Pioneer | Entrepreneur | Investor | Empath | Board Member | Transformational Evangelist | Happy Lady.', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,f_auto,h_595,q_auto:eco/v1523635339/experts/sdo9fgjmzhsjvm2xyqhm.jpg'
+      id: 2, url: 'ben-way', first_name: 'Ben', last_name: 'Way', description: 'Crypto/ICO expert with extensive technical background', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,h_595/v1523898203/experts/byfzhf14drmapbugfq7n.png'
     },
     {
-      id: 3, url: 'nikhilwins', first_name: 'Nikhil', last_name: 'Bhaskar', description: 'Founder & CEO of Ulixir Inc', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,h_595/v1525173789/experts/zpgc4a6ggaocwffdjeue.jpg'
+      id: 3, url: 'baselismail', first_name: 'Basel', last_name: 'Ismail', description: 'Co-Founder and CEO of Blockgram; Blockchain Technology and Cryptocurrencies; Data Scientist', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,h_595/v1523895908/experts/abjd6gteupdspx5t22dg.jpg'
     },
     {
-      id: 4, url: 'baselismail', first_name: 'Basel', last_name: 'Ismail', description: 'Co-Founder and CEO of Blockgram; Blockchain Technology and Cryptocurrencies; Data Scientist', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,h_595/v1523895908/experts/abjd6gteupdspx5t22dg.jpg'
+      id: 4, url: 'nikhilwins', first_name: 'Nikhil', last_name: 'Bhaskar', description: 'Founder & CEO of Ulixir Inc', image: 'https://res.cloudinary.com/dtvc9q04c/image/upload/c_scale,h_595/v1525173789/experts/zpgc4a6ggaocwffdjeue.jpg'
     }
   ];
   return (
