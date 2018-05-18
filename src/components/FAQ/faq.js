807d49ff3fa99da90be83b9c7b65ec2bb419ef5a
j10sanders <jonsandersss@gmail.com>
2018-05-18 19:41:42 -0400

    update faq

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -5,26 +5,30 @@ import Subheader from 'material-ui/Subheader';
 
 const style = { lineHeight: '24px' }
 
+
+
+
 const customerFAQs = [
   [`What is Dimpull?`,
-    `Dimpull is a marketplace for experienced crypto traders and experts to provide their time and knowledge to new crypto ethusiats through phone calls.`],
+    `Dimpull is a platform for people to connect with blockchain experts through paid 1-on-1 phone calls.`],
   [`Why should I use Dimpull?`,
-    `Dimpull connects you with the best experts in the blockchain ecosystem to get firsthand 1-on-1 advice.  Questions about where the market is headed?  Wondering what a 'cup and handle' is, or other TA patterns?  Need advice on a major BTC, BCH, ETH, or LTC purchase?  Ask a Dimpull expert.`],
+    `Dimpull connects you with the best experts in the blockchain ecosystem to get 1-on-1 advice.  Questions about where the BTC, BCH, ETH, or LTC market is headed?  Wondering what a 'cup and handle' is, or other TA patterns?  Need advice on a major BTC, BCH, ETH, or LTC trade?  Need help building a decentralized application?  Ask a Dimpull expert.`],
   [`How do I pay for calls?`,
-    `Calls on Dimpull are paid for with Ether using the MetaMask browser extension.  See below if you want to learn how to purchase Ether.`],
-  [`What is this escrow business?`,
-    `Dimpull uses an escrow smart contract to hold funds during the call.  As a caller you pay into this escrow contract before a call.  Once a call is completed, funds are sent from the escrow contract to the expert.  Using an escrow contract on the blockchain allows payments to go from callers to experts without either user needing to trust Dimpull to hold those funds.  In other words, there is no way for Dimpull to take funds from callers or experts.`],
+    `Calls on Dimpull are paid for with Ether using the MetaMask browser extension.  See the blockchain section if you want to learn how to install MetaMask and purchase Ether.`],
+  [`Why does Dimpull escrow funds?`,
+    `Dimpull uses an escrow smart contract to hold funds during a call.  As a caller you pay into this escrow contract before a call.  Once a call is completed, funds are sent from the escrow contract to the expert.  Using an escrow contract on the blockchain allows payments to go from callers to experts without either user needing to trust Dimpull to hold those funds.  In other words, there is no way for Dimpull to take funds from callers or experts.`],
   [`How much does it cost to use Dimpull?`,
-    `Dimpull takes 18% of payments to experts to run the website and ensure that conversations on the platform meet current legal guidelines.`],
+    `Dimpull takes 18% of payments to experts in order to run the website and ensure that conversations on the platform meet current legal guidelines.`],
+
 ];
 
 const expertFAQs = [
   [`Why should I use Dimpull?`,
-    `Dimpull provides you an easy way for you to connect with people who want your advice and get paid for your time.  Payments are automatic following a call's completion, so you get your hard earned ETH immediately.`],
+    `Dimpull provides an easy way to connect with people who want your advice and to get paid for giving advice.  Payments are automatic following a call's completion, so you get your hard earned ETH immediately :)`],
   [`How do I get paid for calls?`,
-    `On Dimpull, users pay in ETH for your time on the phone.  When you sign up for Dimpull, we ask for the Ethereum address that you want these payments to get sent to.  After a call finishes, ETH payment will automatically get sent to your Ethereum address from our escrow smart contract.  It's that simple!`],
+    `On Dimpull, users pay in ETH for your time on the phone.  When you sign up for Dimpull, we ask for your Ethereum address.  After a call finishes, ETH payment will automatically get sent to your Ethereum address from our escrow smart contract.  It's that simple!`],
   [`What if the caller doesn't want to pay?`,
-    `Callers pay into our escrow contract prior to the start of a call.  As an expert, you are guarenteed payment for calls that take place.`],
+    `Callers pay into our escrow contract prior to the start of a call.  As an expert, you are guaranteed payment for calls that take place.`],
   [`What if a call does not last the full 30 minutes?`,
     `If both parties agree to end a call early, callers still pay for the entirety of the 30 minutes.`],
   [`How does Dimpull make money?`,
@@ -33,13 +37,16 @@ const expertFAQs = [
 
 const blockchainFAQs = [
   [`What is Ethereum?`,
-    `TBD`],
+    `Ethereum is a programmable blockchain that allows developers to decentralize part or all of the applications they build.  The native asset of Ethereum is Ether (aka ETH).`],
   [`What is MetaMask?`,
-    `TBD`],
-  [`How can I get Ether?`,
-    `TBD`],
+    `MetaMask is a digital wallet that holds your ETH.  ETH is a cryptoasset that is used to pay for calls on Dimpull.`],
+  [`How do I install MetaMask?`,
+    `See here: https://youtu.be/tfETpi-9ORs`],
+  [`How can I get Ether (ETH)?`,
+    `If you are a US citizen, you can purchase Ether through MetaMask (with Coinbase).  Just click the "Buy" button in MetaMask.  If you are not a US citizen, you will need to purchase Ether through an exchange and transfer it to your MetaMask wallet.`],
   [`Why is Dimpull using the blockchain?`,
     `Blockchains provide trust.  Using an escrow contract on the blockchain allows payments to go from callers to experts without either user needing to trust Dimpull to hold those funds.  In other words, there is no way for Dimpull to take funds from callers or experts.  You don't need to trust us; trust the blockchain.`],
+
 ];
 
 class FAQ extends Component {
